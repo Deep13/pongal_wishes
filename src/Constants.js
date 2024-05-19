1a706eda07233663fe3d2ed22ce990a957ef8e30
Deep13 <deep.deepak30@gmail.com>
2024-05-19 14:10:43 +0530

    initial

---
+++ src/Constants.js
@@ -0,0 +1,141 @@
+export const day_title = 'Happy Pongal';
+export const banner_key1 = 'ca-app-pub-4717579333914549/8553705843';
+export const banner_key2 = 'ca-app-pub-4717579333914549/1988297499';
+export const banner_key3 = 'ca-app-pub-4717579333914549/2059900945';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/8433737602';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/4494492598';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/4231317451';
+export const language = ['English', 'Tamil'];
+export const textData = {
+  English: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'May the sweetness of jaggery, milk, and these dry fruits bring the sweetest wishes to you and your family. Happy Pongal',
+      },
+      {
+        id: '002',
+        text:
+          'On this festive season may every color of love fill your home and heart with lots of happiness. Happy Pongal',
+      },
+      {
+        id: '003',
+        text:
+          'On this auspicious day of the year do not forget to celebrate the gifts of life. Show your gratitude to the almighty for every blessing you have in your life. Happy Pongal',
+      },
+      {
+        id: '004',
+        text:
+          'May this harvest festival diminish all your worries and fears from your life and fill your heart with calm and healthy thoughts. Happy Pongal',
+      },
+      {
+        id: '005',
+        text:
+          'Let us meet, greet and eat together with this auspicious decoration and beautiful kolams. Wish you a very Happy Pongal',
+      },
+      {
+        id: '006',
+        text:
+          'May this auspicious day brings in good luck to your home and may success touch your feet. Happy Pongal',
+      },
+      {
+        id: '007',
+        text:
+          'Wishing you a prosperous and joyful Pongal. Hope this special day marking the start of a harvest season be happy and successful for you in every way and brings with it prosperity, good luck and moments to cherish. Happy Pongal!',
+      },
+      {
+        id: '008',
+        text:
+          'Wish you a very Happy Pongal, Pot rice to Sun God, Sugarcane to Cow and Ox, Sweet rice to You and Me, Good milk to Friends and Family.',
+      },
+      {
+        id: '009',
+        text:
+          'Pongal marks joy and cheer and brings along everything that’s best. May the festival of harvest season be one that brings along with it all that’s best and everything you deserve. Have a memorable Pongal',
+      },
+      {
+        id: '010',
+        text:
+          'I pray that this festival may be the start of your brighter days filled with happiness, good luck, and prosperity. Happy Pongal 2021.',
+      },
+      {
+        id: '011',
+        text:
+          'Celebrate this day with a heart filled with cheer and fervor. Sending my warmest greetings to you and to your family and loved ones this Pongal. Pongalo Pongal! Happy Pongal !!',
+      },
+      {
+        id: '012',
+        text:
+          'May Parvati shower her blessings, May Lord Ganesha gives his praisings, May all harvests get more yield and our cattle always help us in the field. Happy Ponga 2021!!',
+      },
+    ]
+  }
+  ,
+  Tamil: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'உலகில் உள்ள தமிழர் அனைவருக்கும் இனிய உழவர் திருநாள் வாழ்த்துகள் 2021!',
+      },
+      {
+        id: '002',
+        text:
+          'பானையிலிருந்து பொங்கல் பால் போல, அன்பும் பாசமும் உங்கள் இதயத்திலிருந்து நிரம்பி வழியட்டும். பொங்கலோ பொங்கல்!',
+      },
+      {
+        id: '003',
+        text:
+          'மகிழ்ச்சி, நல்ல அதிர்ஷ்டம் மற்றும் செழிப்பு நிறைந்த உங்கள் பிரகாசமான நாட்களின் தொடக்கமாக இந்த திருவிழா இருக்க வேண்டும் என்று நான் பிரார்த்திக்கிறேன். இனிய பொங்கல்!',
+      },
+      {
+        id: '004',
+        text:
+          'பொங்கலின் இந்த மகிழ்ச்சியான நாளில் சூரியக் கடவுள் தனது அரவணைப்பைப் பொழியட்டும். பொங்கலின் இந்த மகிழ்ச்சியான நாளில் கரும்பு அதன் இனிமையை வழங்கட்டும். அனைவருக்கும் இனிய பொங்கல்!',
+      },
+      {
+        id: '005',
+        text:
+          'சூரியனுக்கு பாட் ரைஸ், பசு மற்றும் எருதுக்கு கடவுள் கரும்பு, உங்களுக்கும் எனக்கும் இனிப்பு உயர்வு, இனிய பொங்கல்!',
+      },
+      {
+        id: '006',
+        text:
+          'பொங்கலின் அறுவடை விழாவை நீங்கள் கொண்டாடும்போது ஆண்டு முழுவதும் உங்கள் வாழ்க்கையில் செழிப்பும் மகிழ்ச்சியும் வளரட்டும்!',
+      },
+      {
+        id: '007',
+        text:
+          'உங்களுக்கு மகிழ்ச்சி, அதிர்ஷ்டம் மற்றும் செழிப்பு ஆகியவற்றைக் கொடுக்கும் ஒரு பொங்கலை விரும்புகிறேன். இனிய பொங்கல்',
+      },
+      {
+        id: '008',
+        text:
+          'உங்கள் பாரம்பரியத்தின் கவர்ச்சியில் நீங்கள் மகிழ்ச்சியடைவீர்கள் என்று நம்புகிறேன்! உங்களுக்கும் உங்கள் குடும்பத்தினருக்கும் மிகவும் இனிய போகி பொங்கல், சூர்ரியா பொங்கல், மாத்து பொங்கல் வாழ்த்துக்கள்!',
+      },
+      {
+        id: '009',
+        text:
+          'சர்வவல்லவர் உங்கள் அனைவருக்கும் சிறந்த ஆரோக்கியம், செல்வம் மற்றும் செழிப்புடன் ஆசீர்வதிப்பாராக. உங்களுக்கு வளமான எதிர்காலம் மற்றும் மகிழ்ச்சியான பொங்கல் வாழ்த்துக்கள்!',
+      },
+      {
+        id: '010',
+        text:
+          'செழிப்பைக் கொண்டாட வேண்டிய நேரம் இது திருவிழா உங்கள் வாழ்க்கையில் மகிழ்ச்சியைத் தரட்டும் ஏராளமான பொங்கலோ பொங்கல் பண்டிகையை கொண்டாடுங்கள்!',
+      },
+      {
+        id: '011',
+        text:
+          'பார்வதி தனது ஆசீர்வாதங்களை பொழியட்டும், விநாயகர் தனது புகழைப் பெறட்டும், எல்லா அறுவடைகளுக்கும் அதிக மகசூல் கிடைக்கட்டும், எங்கள் கால்நடைகள் எப்போதும் வயலில் நமக்கு உதவட்டும். இனிய போங்கா 2021 !!',
+      },
+      {
+        id: '012',
+        text:
+          'இந்த பொங்கல் ஒரு அறுவடை பருவத்தின் தொடக்கத்தை குறிக்கிறது, இது உங்களுக்கு எல்லா வகையிலும் மகிழ்ச்சியாகவும் வெற்றிகரமாகவும் இருக்கிறது, மேலும் இது செழிப்பு, நல்ல அதிர்ஷ்டம் மற்றும் மகிழ்ச்சியான தருணங்களை தருகிறது !!',
+      },
+    ]
+  }
+
+
+};
